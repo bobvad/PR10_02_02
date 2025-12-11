3dbd420741b5687dfed4ac002108e178e23dd351
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-12-11 21:31:31 +0500

    Разработанный метод для получения токена пользователя. Дегтянников.

--- APIGigaChat/Program.cs
+++ APIGigaChat/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using APIGigaChat.Models.Response;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +11,48 @@ namespace APIGigaChat
 {
     internal class Program
     {
+        static string ClientId = "***";
+        static string AuthorizationKey = "***";
         static void Main(string[] args)
         {
+
+        }
+        public static async Task<string> GetToken(string rqUID, string bearer)
+        {
+            string returnToken = null;
+            string url = "https://ngw.devices.sberbank.ru:9WU3/api/v2/oauth";
+
+            using (HttpClientHandler handler = new HttpClientHandler())
+            {
+                handler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) => true;
+
+                using (HttpClient client = new HttpClient(handler))
+                {
+                    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, url);
+
+                    request.Headers.Add("Accept", "application/json");
+                    request.Headers.Add("RqUID", rqUID);
+                    request.Headers.Add("Authorization", $"Bearer {bearer}");
+
+                    var data = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("scope", "GIGACHAT_API_PERS")
+            };
+
+                    request.Content = new FormUrlEncodedContent(data);
+
+                    HttpResponseMessage response = await client.SendAsync(request);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseContent = await response.Content.ReadAsStringAsync();
+                        ResponseToken token = JsonConvert.DeserializeObject<ResponseToken>(responseContent);
+                        returnToken = token.access_token;
+                    }
+                }
+            }
+
+            return returnToken;
         }
     }
 }
